f687e8040e602be9d1740b94b70c54426d47f6d2
Michael Maragos <maragosm@aol.com>
2022-06-21 16:36:14 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncap
+{
+    public class BankAccount
+    {
+        private double Balance = 0;
+        public string AccountID { get; set; }
+
+        public void Deposit(double amount)
+        {
+            Balance = Balance + amount;
+            Console.WriteLine($"You have successfully deposited ${amount} to your account. Your new balance is {Balance}");
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance = Balance - amount;
+            if (Balance > 0)
+            {
+                Console.WriteLine($"You have successfully withdrew ${amount} to your account. Your new balance is {Balance}");
+            }
+            else if (Balance < 0)
+            {
+                var loan = Balance;
+                Console.WriteLine($"You have successfully taken out a loan for {loan} and your Balance is $0.");
+                Balance = 0;
+                Console.WriteLine($"Current interest rates are compounding at 38.4%, enjoy your life of debt!");
+            }
+            
+        }
+
+        public void BalanceCheck()
+        {
+            Console.WriteLine($"Your current balance is ${Balance}");
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BankEncap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the First National Bank of C#");
+            Console.WriteLine("To continue, enter your account ID: ");
+            var account1 = new BankAccount() { AccountID = Console.ReadLine() };
+            Console.WriteLine("Press 1 to view your balance. Press 2 to make a deposit. Press 3 to make a withdrawal.");
+            Console.Write("Action: ");
+            var userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                case "1":
+                    account1.BalanceCheck();
+                    break;
+                case "2":
+                    Console.WriteLine("How many dollars would you like to deposit?");
+                    Console.Write("Amount: ");
+                    var depoAmount = Convert.ToInt32(Console.ReadLine());
+                    account1.Deposit(depoAmount);
+                    break;
+                case "3":
+                    Console.WriteLine("How many dollars would you like to withdraw?");
+                    Console.Write("Amount: ");
+                    var withdrawAmount = Convert.ToInt32(Console.ReadLine());
+                    account1.Withdraw(withdrawAmount);
+                    break;
+                default:
+                    do
+                    {
+                        Console.WriteLine("Please enter a valid input to continue");
+                        Console.Write("Input: ");
+                        userInput = Console.ReadLine();
+                    } while (userInput != "1" && userInput != "2" && userInput != "3");
+                    break;
+            }
+
+            Console.WriteLine("Thank you for using the First National Bank of C# application!");
+
+        }
+    }
+}
